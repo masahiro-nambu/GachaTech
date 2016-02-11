4ccfe7826b5577f7a2a307d879c45f119f76f212
南部匡寛 <m.nambu1121@gmail.com>
2016-02-11 17:15:02 +0900

    first commit

---
+++ GachaTech/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GachaTech
+//
+//  Created by 南部　匡寛 on 2016/02/11.
+//  Copyright © 2016年 南部匡寛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func gacha() {
+        self.performSegueWithIdentifier("result", sender: nil)
+    }
+
+
+}
+
